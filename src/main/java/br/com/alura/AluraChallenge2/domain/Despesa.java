18f3c29909eb1db44b994ea4b56e818295f4b4e8
Diego Brito <diego.brito@f1rst.com.br>
2023-04-11 20:52:40 -0300

    Add builder to domains

--- src/main/java/br/com/alura/AluraChallenge2/domain/Despesa.java
+++ src/main/java/br/com/alura/AluraChallenge2/domain/Despesa.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.Table;
@@ -12,6 +13,7 @@ import org.hibernate.annotations.Table;
 @Table(appliesTo = "despesa")
 @Getter
 @Setter
+@Builder
 public class Despesa {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/br/com/alura/AluraChallenge2/domain/Receita.java
+++ src/main/java/br/com/alura/AluraChallenge2/domain/Receita.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.Table;
@@ -13,6 +14,7 @@ import org.hibernate.annotations.Table;
 @Table(appliesTo = "receita")
 @Getter
 @Setter
+@Builder
 public class Receita {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
