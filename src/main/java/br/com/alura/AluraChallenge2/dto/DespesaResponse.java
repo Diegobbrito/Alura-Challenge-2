3bb64c9a088840fd97973a94c9b797669c32857e
Diego Brito <diego.brito@f1rst.com.br>
2023-04-12 11:07:32 -0300

    Add dtos to response

---
+++ src/main/java/br/com/alura/AluraChallenge2/dto/DespesaResponse.java
@@ -0,0 +1,18 @@
+package br.com.alura.AluraChallenge2.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Builder
+public class DespesaResponse {
+    private String descricao;
+    private String valor;
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate data;
+}
