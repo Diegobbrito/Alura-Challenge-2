55a4e1268bf1121d6812c5f88a9c1486d3c58017
Diego Brito <diego.brito@f1rst.com.br>
2023-04-12 11:08:33 -0300

    Add validations and change data type

--- src/main/java/br/com/alura/AluraChallenge2/dto/DespesaRequest.java
+++ src/main/java/br/com/alura/AluraChallenge2/dto/DespesaRequest.java
@@ -1,12 +1,21 @@
 package br.com.alura.AluraChallenge2.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDate;
 
 @Getter
 @Setter
 public class DespesaRequest {
+    @NotBlank
     private String descricao;
+    @NotBlank
     private String valor;
-    private String data;
+    @NotNull
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate data;
 }
--- src/main/java/br/com/alura/AluraChallenge2/dto/ReceitaRequest.java
+++ src/main/java/br/com/alura/AluraChallenge2/dto/ReceitaRequest.java
@@ -1,13 +1,22 @@
 package br.com.alura.AluraChallenge2.dto;
 
-import lombok.Builder;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
 
 @Getter
 @Setter
 public class ReceitaRequest {
+    @NotBlank
     private String descricao;
+    @NotBlank
     private String valor;
-    private String data;
+    @NotNull
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate data;
 }
